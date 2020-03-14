953b9ddd52866133690149d759ae2c174fe46804
Veli Koskinen <veli.koskinen@halton.com>
2020-03-14 10:30:04 +0200

    1

---
+++ public/mqtt-threejs.js
@@ -0,0 +1,25 @@
+let three_view = document.getElementById('three_view');
+
+let renderer = new TRHEE.WebGLRenderer();
+let scene = new THREE.scene();
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+let view_angle = 45;
+let near = 0.1; //yksikkö metriä
+let far = 1000;
+
+//Camera setup
+let camera = new TRHEE.perspectiveCamera(view_angle, width / height, near, far);
+camera.position.x = 5;
+camera.position.y = 5;
+camera.position.z = 5;
+
+renderer.setSize(width, height);
+three_view.appendChild(renderer.domElement);
+
+const animate = ()=>{
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+animate();
